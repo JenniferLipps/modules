6b0932b7236aca43cea794772b3a59f863afadca
Jennifer Lipps <jjlipps@gmail.com>
2019-04-08 21:26:17 -0500

    added store component, prints to page

---
+++ javascripts/helpers/book.js
@@ -0,0 +1,11 @@
+const bookInfo = {
+    price: 24.99,
+    title: 'Catcher in the Rye',
+    image: './assets/book.jpg'
+};
+
+const getBook = () => {
+    return bookInfo;
+};
+
+export default { getBook };
